31f250a00d4fd743c9d5f8cfe6d9afb3d6a46037
andrewjbotros <andrewjbotros@gmail.com>
2014-02-14 20:54:40 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackerHouse::Application.config.secret_key_base = '72c3334fa535bd0a3e62fad61e516fbe802145dccf00f8170959babc5156e07f6cdcf8ba5c48cae58a7cde919d0e634f1c199e8e69dbdbbe250aee7824a4dadc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HackerHouse::Application.config.session_store :cookie_store, key: '_hacker_house_session'
